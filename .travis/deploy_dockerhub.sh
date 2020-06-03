6f1afa75196336fc1c52430a2db954103b2a6fe6
Andre Boyce <mobile@andreboyce.com>
2020-06-03 09:31:08 -0400

    Testing docker push in Travis CI

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+echo "TRAVIS_REPO_SLUG $TRAVIS_REPO_SLUG"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-echo "TRAVIS_REPO_SLUG $TRAVIS_REPO_SLUG"
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
